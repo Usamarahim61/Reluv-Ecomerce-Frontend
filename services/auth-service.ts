7259c03fa77f262ec39603a4c01e85f69db0cd03
rajaabad335 <rajaabad335@gmail.com>
2026-03-01 14:42:23 +0500

    set the account settingds component to update data

--- services/auth-service.ts
+++ services/auth-service.ts
@@ -14,7 +14,6 @@ export function register(username: string, email: string, password: string) {
   });
 }
 export function getUser(id: number) {
-  // &populate[avatar]=*&populate[received_reviews][populate]=author
   return apiRequest(
     `/users/${id}?populate[products][populate]=*&populate[role]=*&populate[received_reviews][populate]=author&populate[following][populate]=*&populate[followers][populate]=*`, 
     {
@@ -22,53 +21,24 @@ export function getUser(id: number) {
     }
   );
 }
-// export function updateUserProfile(id: number, data: FormData) {
-// return apiRequest(`/users/${id}`, {
-//     method: "PUT",
-//     body: data, 
-//     headers: {
-//       // This overrides the "application/json" inside apiRequest
-//       // Letting the browser automatically handle the multipart/form-data boundary
-//       "Content-Type": undefined as any, 
-//     },
-//   });
-// }
-export async function updateUserProfile(id: number, userData: any) {
-  let avatarId = null;
-
-  // 1. If there is a new file, upload it to /api/upload first
-  if (userData.files?.avatar) {
-    const fileData = new FormData();
-    fileData.append("files", userData.files.avatar);
-
-    const uploadRes = await apiRequest("/upload", {
-      method: "POST",
-      body: fileData,
-      headers: { "Content-Type": undefined as any }, // Let browser set boundary
-    });
-
-    // uploadRes is an array of file objects; get the ID of the first one
-    avatarId = uploadRes[0]?.id;
-  }
-
-  // 2. Now update the User Profile with the text data + the new Avatar ID
-  const updatePayload = {
-    username: userData.username,
-    country: userData.country,
-    city: userData.city,
-    description: userData.description,
-    isShowCity: userData.isShowCity,
-    // Connect the uploaded file ID to the user's avatar field
-    ...(avatarId ? { avatar: avatarId } : {}),
-  };
-
+export function AccountUpdate(id: number, data: any) {
   return apiRequest(`/users/${id}`, {
     method: "PUT",
-    body: JSON.stringify(updatePayload),
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json", // Tells the server you're sending JSON
+    },
+    body: JSON.stringify(data), // Converts your object to a JSON string
   });
 }
-
+export function updateUserProfile(id: number, data: FormData) {
+return apiRequest(`/users/${id}`, {
+    method: "PUT",
+    body: data, 
+    headers: {
+      "Content-Type": "application/json", 
+    },
+  });
+}
 export function logout() {
   localStorage.removeItem("jwt");
   localStorage.removeItem("user");
