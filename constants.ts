c7df9171741c55c078fdd2d53c51df9d67085ad2
Usamarahim61 <usamarahim61@gmail.com>
2026-05-13 19:57:50 +0500

    change backend url

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
-// export const BACKEND_URL = "https://reluv-ecomerce-backend-1.onrender.com";
+export const BACKEND_URL = "https://reluv-ecomerce-backend-1.onrender.com";
 // export const BACKEND_URL = "http://192.168.100.134:1337";
-export const BACKEND_URL = "http://localhost:1337";
+// export const BACKEND_URL = "http://localhost:1337";
+
