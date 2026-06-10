e1a90d2cb102dd0e6b32fb734406c7b1486bda01
Usamarahim61 <usamarahim61@gmail.com>
2026-06-10 11:45:05 +0500

    change backend url

--- constants.ts
+++ constants.ts
@@ -1,7 +1,8 @@
-export const BACKEND_URL = "https://reluv-ecomerce-backend-1.onrender.com";
+// export const BACKEND_URL = "https://reluv-ecomerce-backend-1.onrender.com";
 // export const BACKEND_URL = "https://wealthy-authority-eb78443cb3.strapiapp.com";
 // export const BACKEND_URL = "http://192.168.100.134:1337";
 // export const BACKEND_URL = "http://localhost:1337";
+export const BACKEND_URL = "http://193.123.94.110:1337";
 
 export const NEXT_PUBLIC_GOOGLE_CLIENT_ID="793055192577-592s2th4cdsjoclel6il7a5lbqs954dc.apps.googleusercontent.com"
 export const NEXT_PUBLIC_FACEBOOK_APP_ID="your_facebook_app_id_here"
