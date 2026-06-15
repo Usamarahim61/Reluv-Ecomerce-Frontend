129bb7786fe655aee28bf487dfd04391c10375ac
Usamarahim61 <usamarahim61@gmail.com>
2026-06-15 11:41:13 +0500

    minor fix

--- constants.ts
+++ constants.ts
@@ -1,8 +1,8 @@
 // export const BACKEND_URL = "https://reluv-ecomerce-backend-1.onrender.com";
 // export const BACKEND_URL = "https://wealthy-authority-eb78443cb3.strapiapp.com";
 // export const BACKEND_URL = "http://192.168.100.134:1337";
-export const BACKEND_URL = "http://localhost:1337";
-// export const BACKEND_URL = "https://reluv.novakonnect.com";
+// export const BACKEND_URL = "http://localhost:1337";
+export const BACKEND_URL = "https://reluv.novakonnect.com";
 
 export const NEXT_PUBLIC_GOOGLE_CLIENT_ID="793055192577-592s2th4cdsjoclel6il7a5lbqs954dc.apps.googleusercontent.com"
 export const NEXT_PUBLIC_LINE_CHANNEL_ID="439298713ffdc12df434a03e13893276"
