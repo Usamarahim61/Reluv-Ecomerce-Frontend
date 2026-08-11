ccdea247ff6b6b108d4e6ae3a153e5b4ee819968
Reluv Dev <usamarahim61@gmail.com>
2026-08-12 00:50:21 +0500

    fix

--- constants.ts
+++ constants.ts
@@ -4,7 +4,5 @@
 // export const BACKEND_URL = "http://localhost:1337";
 export const BACKEND_URL = "https://reluv.novakonnect.com";
 
-export const NEXT_PUBLIC_GOOGLE_CLIENT_ID="793055192577-592s2th4cdsjoclel6il7a5lbqs954dc.apps.googleusercontent.com"
-export const NEXT_PUBLIC_LINE_CHANNEL_ID="2011055237"
 export const NEXT_PUBLIC_LINE_CALLBACK_URL="https://reluv-ecomerce-frontend.vercel.app/auth/callback/line"
 export const NEXT_PUBLIC_SITE_URL="https://reluv-ecomerce-frontend.vercel.app"
