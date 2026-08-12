a6e44498c4b15adc3116eb7c9713968d9163ec3e
Reluv Dev <usamarahim61@gmail.com>
2026-08-12 08:54:45 +0500

    minor change

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL ?? "https://reluv.novakonnect.com"
 
-export const NEXT_PUBLIC_GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ""
-export const NEXT_PUBLIC_LINE_CHANNEL_ID = process.env.NEXT_PUBLIC_LINE_CHANNEL_ID ?? ""
-export const NEXT_PUBLIC_LINE_CALLBACK_URL = process.env.NEXT_PUBLIC_LINE_CALLBACK_URL ?? ""
-export const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? ""
+export const NEXT_PUBLIC_GOOGLE_CLIENT_ID = "793055192577-592s2th4cdsjoclel6il7a5lbqs954dc.apps.googleusercontent.com"
+export const NEXT_PUBLIC_LINE_CHANNEL_ID = "2011055237"
+export const NEXT_PUBLIC_LINE_CALLBACK_URL = "https://reluv-ecomerce-frontend.vercel.app/auth/callback/line"
+export const NEXT_PUBLIC_SITE_URL = "https://reluv-ecomerce-frontend.vercel.app"
